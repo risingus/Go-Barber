33e22e5d6df055cf5d373c112cd6697070f63368
Gustavo Lima <gustavo.it@outlook.com>
2020-10-07 23:58:21 -0300

    created code for create table user on database. 2. Created relation with the table appointments

--- src/database/migrations/1602094772831-CreateUsers.ts
+++ src/database/migrations/1602094772831-CreateUsers.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1601992029855 implements MigrationInterface {
+export default class CreateUsers1602094772831 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                 {
                     name: 'id',
@@ -15,12 +15,17 @@ export default class CreateAppointments1601992029855 implements MigrationInterfa
                     default: 'uuid_generate_v4()',
                 },
                 {
-                    name: 'provider',
+                    name: 'name',
                     type: 'varchar',
                 },
                 {
-                    name: 'date',
-                    type: 'timestamp with time zone',
+                    name: 'email',
+                    type: 'varchar',
+                    isUnique: true,
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
                 },
                 {
                     name: 'created_at',
@@ -32,13 +37,14 @@ export default class CreateAppointments1601992029855 implements MigrationInterfa
                     type: 'timestamp',
                     default: 'now()',
                 },
+
                 ]
             })
         )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 
 }
