7d8e1b4255fe10ba30455470fc03edce3599047c
Gustavo Lima <gustavo.it@outlook.com>
2020-10-15 11:02:15 -0300

    Adicionada autenticação de usuário. 2. Gerado token jwt.

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import {getRepository} from 'typeorm';
+import User from '../models/User';
+import {compare} from 'bcryptjs';
+import {sign} from 'jsonwebtoken';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response{
+    user: User;
+    token: string;
+}
+
+class AuthenticateUserService{
+    public async execute({email, password}: Request): Promise<Response>{
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({where: {email}});
+
+        if (!user) {
+            throw new Error('Incorrect email/password combination.')
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched){
+            throw new Error('Incorrect email/password combination.')
+        }
+
+        const token = sign({}, '8538607221f2e42284acf599214cfa34', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token,
+        };
+
+    }
+}
+
+export default AuthenticateUserService;
